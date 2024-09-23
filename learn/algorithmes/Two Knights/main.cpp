410ad5c0b3a34b59bc1647f74498888142273826
Sidak <magicalsidak@gmail.com>
2024-09-23 19:04:56 -0400

    did the Two Knights problem kinda

---
+++ learn/algorithmes/Two Knights/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate and print the number of ways two
+// knights can be placed on a K X K chessboard such that
+// they do not attack each other
+long calculateWays(int K) {
+    // Total number of ways two knights can be placed on
+    // the chessboard
+    long totalWays = ((long) K * K * (K * K - 1)) / 2;
+
+    // Number of ways two knights can attack each other
+    long attackingWays = 4 * (K - 1) * (K - 2);
+
+    // Number of ways two knights can be placed without
+    // attacking each other
+    long ans = totalWays - attackingWays;
+
+    // Return the result for the current chessboard size K
+    return ans;
+}
+
+// Driver Code
+int main() {
+    // Input the value of N (size of the chessboard)
+    int N;
+    cin >> N;
+
+    // Iterate for all the K sized chessboard
+    for (int K = 1; K <= N; K++) {
+        cout << calculateWays(K) << " ";
+    }
+
+    return 0;
+}
