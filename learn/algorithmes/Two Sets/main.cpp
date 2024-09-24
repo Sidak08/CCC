c6419c99a93125f9cf55567cf8a66fcc86160699
Sidak <magicalsidak@gmail.com>
2024-09-24 18:07:39 -0400

    did CSES Problem Set Two Sets and passed all the test cases

--- learn/algorithmes/Two Sets/main.cpp
+++ learn/algorithmes/Two Sets/main.cpp
@@ -5,44 +5,28 @@ using namespace std;
 
 
 void calcSet (ll num) {
-    if (num % 2 == 0) {
-        if ((num / 2) % 2 == 0){
-        cout << num / 2 << "\n";
-        for (ll i = 0; i < num / 2; i+=2) {
-            cout << num - i << " ";
-            cout << i + 1 << " ";
-        }
-        cout << "\n" << num / 2 << "\n";
-        for (ll i = 1; i < num / 2; i+=2) {
-            cout << num - i << " ";
-            cout << i + 1 << " ";
-        }
-        }
-        else {
-            cout << "NO";
-        }
-    }
-    else if (((num * (num + 1)) / 2) % 2 == 0){
+    if (((num * (num + 1)) / 2) % 2 == 0){
         ll dsVal = (num * (num + 1)) / 4;
         string sVal = "";
         string uVal = "";
+        ll sSum = 0;
+        ll uSum = 0;
         for (ll i = num; i > 0; i--){
             if (i <= dsVal){
                 dsVal -= i;
                 uVal += to_string(i) + " ";
+                uSum ++;
             } else {
-                if (!(i == 0)) {
                     sVal += to_string(i) + " ";
-                }
+                    sSum++;
             }
         }
-        cout << "YES" << "\n" << uVal.size() / 2 << "\n" << uVal << "\n" << sVal.size() / 2 << "\n" << sVal;
-
-    }
-    else {
-         cout << "NO";
+        cout << "YES" << "\n" << uSum << "\n" << uVal << "\n" << sSum << "\n" << sVal;
+    } else {
+        cout << "NO";
     }
 }
+
 int main () {
     ll num;
     cin >> num;
