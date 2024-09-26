ef0b108ce93bb9a01db4c85d04bd979252a0fe86
Sidak <magicalsidak@gmail.com>
2024-09-26 17:35:42 -0400

    Coin Piles fixed it but it still does not work

--- cses/Introductory Problems/Coin Piles/main.cpp
+++ cses/Introductory Problems/Coin Piles/main.cpp
@@ -2,26 +2,29 @@
 #define ll long long
 using namespace std;
 
-
-void solv (ll num1, ll num2) {
-    if (num1 >= num2) {
-        ll bgT = num2 / 3;
-        int r1 = num1 - (3 * bgT);
-        int r2 = num2 - (3 * bgT);
-        if (r1 == 1 && r2 == 2 || r1 == 2 && r2 == 1 || r1 == 0 && r2 == 0) {
-            cout << "YES" << "\n";
+void solvRecur(ll num1, ll num2) {
+    while (num1 > 0 || num2 > 0) {
+        if (num1 >= 2 && num2 >= 1) {
+            num1 -= 2;
+            num2 -= 1;
+        } else if (num1 >= 1 && num2 >= 2) {
+            num1 -= 1;
+            num2 -= 2;
         } else {
             cout << "NO" << "\n";
+            return;
         }
     }
+    cout << "YES" << "\n";
 }
-int main () {
+
+int main() {
     ll num;
     cin >> num;
-    for (ll i = 0; i < num; i++){
+    for (ll i = 0; i < num; i++) {
         ll x, y;
         cin >> x >> y;
-        solv(x, y);
+        solvRecur(x, y);
     }
     return 0;
 }
