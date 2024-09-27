e8b777cf2c9e323ffb69064f61d8f8e11f986fe9
Sidak <magicalsidak@gmail.com>
2024-09-27 08:17:08 -0400

    going to chnage directions for Palindrome Reor er and use a hash set

--- cses/Introductory Problems/Palindrome Reorder/main.cpp
+++ cses/Introductory Problems/Palindrome Reorder/main.cpp
@@ -6,23 +6,30 @@
 using namespace std;
 
 void panidrome(string str) {
-    cout << str <<'\n';
+    // cout << str <<'\n';
     ll len = str.size();
     string corStr(len, ' ');
-    for (ll i = 0; i < len - 1 ; i+= 2) {
+    ll counter = 0;
+    for (ll i = 0; i < len; i+=2) {
         if (str[i] == str[i + 1]){
-            corStr[i] = str[i];
-            corStr[len - i] = str[i];
+            corStr[counter] = str[i];
+            corStr[len - counter - 1] = str[i];
         } else if (!(len % 2 != 0 && ((i - 1) != (len / 2) + 1))){
             cout << "NO SOLUTION";
+            return;
+        } else {
+            corStr[i] = str[i];
         }
+        cout << str << " " << corStr << " " << i << " " << corStr[counter] << " " << corStr[len - counter] << "\n";
+        counter++;
     }
     cout << corStr;
 }
 
 int main() {
     string myString;
-    getline(cin, myString);
+    // getline(cin, myString);
+    myString = "AAAACACBA";
     sort(myString.begin(), myString.end());
     panidrome(myString);
     return 0;
