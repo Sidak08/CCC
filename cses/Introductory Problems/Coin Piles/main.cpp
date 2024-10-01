94c93e1396a7373bd56ae659cd8fd309b639dbe6
Sidak <magicalsidak@gmail.com>
2024-10-01 19:54:53 -0400

    Coin Piles works

--- cses/Introductory Problems/Coin Piles/main.cpp
+++ cses/Introductory Problems/Coin Piles/main.cpp
@@ -1,22 +1,31 @@
+#include <cmath>
 #include <iostream>
 #define ll long long
 using namespace std;
 
 void solvRecur(ll num1, ll num2) {
-    while (num1 > 0 || num2 > 0) {
-        if (num1 >= 2 && num2 >= 1) {
-            num1 -= 2;
-            num2 -= 1;
-        } else if (num1 >= 1 && num2 >= 2) {
-            num1 -= 1;
-            num2 -= 2;
+    ll big, small;
+    if (num1 > num2) {
+         big = num1;
+        small = num2;
+    } else {
+        big = num2;
+        small = num1;
+    }
+    ll x = big - small;
+        x = small - x;
+    if (x < 0) {
+        cout << "NO" << "\n";
+    } else {
+        if (x % 3 == 0) {
+            cout << "YES" << "\n";
         } else {
             cout << "NO" << "\n";
-            return;
         }
     }
-    cout << "YES" << "\n";
 }
+
+//
 
 int main() {
     ll num;
