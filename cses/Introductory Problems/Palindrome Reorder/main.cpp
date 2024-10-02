e3d205fed753dee639a0514fe46bfcb00f5794da
Sidak <magicalsidak@gmail.com>
2024-10-01 21:50:26 -0400

    finally finish the Palindrome Reorder

--- cses/Introductory Problems/Palindrome Reorder/main.cpp
+++ cses/Introductory Problems/Palindrome Reorder/main.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
 #include <string>
-#include <algorithm> // For std::sort
+#include <unordered_map>
 #define ll long long
 
 using namespace std;
 
-void panidrome(string str) {
-    // cout << str <<'\n';
-    ll len = str.size();
-    string corStr(len, ' ');
-    ll counter = 0;
-    for (ll i = 0; i < len; i+=2) {
-        if (str[i] == str[i + 1]){
-            corStr[counter] = str[i];
-            corStr[len - counter - 1] = str[i];
-        } else if (!(len % 2 != 0 && ((i - 1) != (len / 2) + 1))){
-            cout << "NO SOLUTION";
-            return;
+void panidrome(unordered_map<char, ll>& alaphaSet, int len) {
+double oddPair = -1;
+string oddPairItem;
+string corStr(len, ' ');
+ll counter = 0;
+ //cout << pair.first << ": " << pair.second << endl;
+
+    for (const auto& pair : alaphaSet) {
+        // cout << oddPair << "\n";
+        if (pair.second % 2 != 0) {
+            oddPair += 1;
+            oddPairItem = pair.first;
         } else {
-            corStr[i] = str[i];
+                for (int i = 0; i < pair.second / 2; i++){
+                    corStr[counter] = pair.first;
+                    corStr[len - counter - 1] = pair.first;
+                    // cout << "\n" << counter << len - counter - 1;
+                    counter++;
+                }
+            }
         }
-        cout << str << " " << corStr << " " << i << " " << corStr[counter] << " " << corStr[len - counter] << "\n";
-        counter++;
+        if (oddPair == 0) {
+            for (int i = 0; i < (alaphaSet[oddPairItem[0]] + 1) / 2; i++){
+                corStr[(len / 2) + i] = oddPairItem[0];
+                corStr[(len / 2) - i] = oddPairItem[0];
+            }
+        }
+        if (oddPair > 0) {
+                cout << "NO SOLUTION" << "\n";
+                return;
+        }
+     cout << corStr;
+}
+
+
+void genHashSet (string myStr, unordered_map<char, ll>& alaphaSet) {
+    for (ll i = 0; i < myStr.size(); i++){
+        alaphaSet[myStr[i]]++;
     }
-    cout << corStr;
+    return;
 }
 
 int main() {
     string myString;
-    // getline(cin, myString);
-    myString = "AAAACACBA";
-    sort(myString.begin(), myString.end());
-    panidrome(myString);
+    getline(cin, myString);
+    // myString = "ABABABABAB";
+    unordered_map<char, ll> alaphaSet;
+    genHashSet(myString, alaphaSet);
+    // for (const auto& pair : alaphaSet) {
+    //     cout << pair.first << ": " << pair.second << endl;
+    // }
+    panidrome(alaphaSet, myString.size());
     return 0;
 }
-
-
-// 10100
-// 1110
