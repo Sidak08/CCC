81b0a7e5a78744cc0fe0b66b4b34310c06a92e5e
Sidak <magicalsidak@gmail.com>
2024-10-21 20:08:19 -0400

    more compsci homework

---
+++ homework/unitTwo/2.3.5.py
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------------------------------------------------
+'''
+Title: 2.3 Numbers in a Range - B)Investigate And Modify
+Author: Sidak Singh
+Date: 2024-10-18
+Assignment: 2.3 Numbers in a Range - B)Investigate And Modify
+'''
+#-------------------------------------------------------------------------------------------------------------------
+#No Imports
+#-------------------------------------------------------------------------------------------------------------------
+#No Globals
+#-------------------------------------------------------------------------------------------------------------------
+#No Functions
+#-------------------------------------------------------------------------------------------------------------------
+birthMonth = int(input("Enter the number of the month in which you were born: "))
+
+if birthMonth < 1 or birthMonth > 12:
+    print(f"{birthMonth} is not a valid month!")
+else:
+    print("Thanks")
+
+# What is the Boolean operator in the code?
+# The Boolean operator is used to combine two conditions.
+
+# What does <= mean?
+# The operator <= means less than or equal to when comparing the left side to the right.
+
+# What would the effect be of changing >= 1 to > 1 on line 7?
+# Changing >= 1 to > 1 would mean that the input of 1 would no longer be considered a valid month.
+
+'''
+No Test Cases
+'''
