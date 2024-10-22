fa07dbd2f77a2fd1128caa65f14e91df6648e03e
Sidak <magicalsidak@gmail.com>
2024-10-21 20:11:08 -0400

    more comp sci homework

---
+++ homework/unitTwo/2.3.6.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------------------------------------------
+'''
+Title: 2.3 Nesting - a) Predict And Run
+Author: Sidak Singh
+Date: 2024-10-21
+Assignment: 2.3 Nesting - a) Predict And Run
+'''
+#-------------------------------------------------------------------------------------------------------------------
+#No Imports
+#-------------------------------------------------------------------------------------------------------------------
+#No Globals
+#-------------------------------------------------------------------------------------------------------------------
+#No Functions
+#-------------------------------------------------------------------------------------------------------------------
+# Assign value 45 to variable num1
+num1 = 45
+
+# if num1 is greater than 20
+if num1 > 20:
+    # num1 greater than 20, print this
+    print("Bigger than 20")
+
+    # also greater than 50
+    if num1 > 50:
+        # If so than print this
+        print(" and bigger than 50")
+    else:
+        # If not greater than 50, print this
+        print("but not bigger than 50")
+else:
+    # If not greater than 20, print this
+    print("Smaller than 20")
+
+'''
+No Test Cases
+'''
